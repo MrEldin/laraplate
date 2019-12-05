2d98baf33d9f8d5e61951a8150e5676731cd3ec5
eldin <eldin@renheadtechnology.com>
2019-12-05 13:36:12 +0100

    feat(laraplate): Add main controller file.

---
+++ src/Api/V1/Controllers/Controller.php
@@ -0,0 +1,29 @@
+<?php
+namespace SmartlyJobs\Api\V1\Controllers;
+use Dingo\Api\Routing\Helpers;
+use Illuminate\Routing\Controller as BaseController;
+/**
+ * @SWG\Swagger(
+ *   basePath="/api",
+ *   @SWG\Info(
+ *     title="SMATRLYJOBS API DOCUMENTATION",
+ *     version="1.0.0",
+ *   ),
+ *     @SWG\SecurityScheme(
+ *         securityDefinition="jwt",
+ *         in="header",
+ *         name= "Authorization",
+ *         type="apiKey",
+ *         description="add jwt token with Bearer ",
+ *         flow="implicit",
+ *         authorizationUrl="/auth/login",
+ *         scopes={"all": "full access with token."}
+ *     )
+ *  )
+ * This class should be parent class for other API controllers
+ * Class ApiController
+ */
+class Controller extends BaseController
+{
+    use Helpers;
+}
